e2a65ffe5a866951d83bac370dbcbaaa7ab836a7
HFooladi <fooladi.hosein@gmail.com>
2024-02-27 18:18:25 +0100

    add distance class

---
+++ themap/data/distance.py
@@ -0,0 +1,16 @@
+
+
+class DatasetDistance():
+    def __init__(self, D1=None, D2=None, method="euclidean"):
+        self.source = D1
+        self.target = D2
+        self.method = method
+        self.prob_network = None
+
+    def get_distance(self):
+        if self.method == "euclidean":
+            return self.euclidean_distance()
+        elif self.method == "cosine":
+            return self.cosine_distance()
+        else:
+            raise ValueError("Invalid distance method")
