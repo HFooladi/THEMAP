3a81871765d2db0405456d856aa7b26cc058b353
HFooladi <fooladi.hosein@gmail.com>
2025-03-14 16:21:30 +0100

    minor versioning change

--- themap/__init__.py
+++ themap/__init__.py
@@ -1,2 +1,8 @@
-
 from ._version import __version__
+
+from . import data
+from . import models
+from . import utils
+from . import metalearning
+
+__all__ = ["data", "models", "utils", "metalearning"]
